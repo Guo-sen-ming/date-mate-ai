14b230b6f414b91f3b0724bb01b2276bd9fce75e
leoguo <leoguo@88vip.site>
2026-03-16 20:50:44 +0800

    feat: add click-to-react-component for dev source inspection

--- date-mate-client/src/App.tsx
+++ date-mate-client/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { Theme } from '@radix-ui/themes'
+import { ClickToComponent } from 'click-to-react-component'
 import '@radix-ui/themes/styles.css'
 import { store } from '@/store'
 import router from '@/router'
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <Theme accentColor="ruby" radius="medium" scaling="100%">
+        <ClickToComponent />
         <RouterProvider router={router} />
       </Theme>
     </Provider>
